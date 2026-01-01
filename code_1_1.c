e764e36f1b2ad48de3d31dbc94d6c31448c65d5d
Gargee Kakaty <k.gargee@op.iitg.ac.in>
2026-01-01 10:33:25 +0530

    Add comment for uppercase/lowercase check
    Code is checking if the entered character is uppercase or lowercase

---
+++ code_1_1.c
@@ -0,0 +1,14 @@
+//a code to check if entered number is uppercase or lowercase
+
+#include<stdio.h>
+int main(){
+    char a;
+    printf("Enter a character: ");
+    scanf("%c",&a);
+    if (a>='A' && a<='Z'){
+        printf("Entered character is an uppercase character.");
+    } else {
+        printf("Entered character is a lowercase character.");
+    }
+    return 0;
+}
