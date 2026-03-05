6f759b9a9858eb9fc9f8957bb98c437c2eeabd6e
Gargee Kakaty <k.gargee@op.iitg.ac.in>
2026-03-05 10:29:46 +0530

    Java program to count vowels in a string

---
+++ Java /code_1_3.java
@@ -0,0 +1,18 @@
+// 3. Count Vowels and Consonants in a String
+
+public class ques3 {
+    public static void main(String[] args) {
+        String str = "Hello World";
+        str = str.toLowerCase();
+        int vowels = 0, consonants = 0;
+
+        for (char ch : str.toCharArray()) {
+            if (ch >= 'a' && ch <= 'z') {
+                if ("aeiou".indexOf(ch) != -1) vowels++;
+                else consonants++;
+            }
+        }
+        System.out.println("Vowels: " + vowels);
+        System.out.println("Consonants: " + consonants);
+    }
+}
