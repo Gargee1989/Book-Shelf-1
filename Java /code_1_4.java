ba4fb38791b5dda704251652ac548035cf04eb60
Gargee Kakaty <k.gargee@op.iitg.ac.in>
2026-03-06 16:06:52 +0530

    Java program anagram checker for two strings

---
+++ Java /code_1_4.java
@@ -0,0 +1,21 @@
+// 4. Determine if Two Strings Are Anagrams
+
+import java.util.Arrays;
+public class ques5 {
+    public static void main(String[] args) {
+        String str1 = "listen";
+        String str2 = "silent";
+
+        char[] arr1 = str1.toCharArray();
+        char[] arr2 = str2.toCharArray();
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        if (Arrays.equals(arr1, arr2)) {
+            System.out.println("Anagrams");
+        } else {
+            System.out.println("Not Anagrams");
+        }
+    }
+}
